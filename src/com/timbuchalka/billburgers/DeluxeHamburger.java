5e420bb825763089703ae7cc63808c324898257c
piotrgy <piotr_gy@wp.pl>
2017-08-30 20:56:55 +0200

    Now logic is corrected.

--- src/com/timbuchalka/billburgers/DeluxeHamburger.java
+++ src/com/timbuchalka/billburgers/DeluxeHamburger.java
@@ -23,7 +23,7 @@ public class DeluxeHamburger extends Hamburger {
     }
 
     public void addChips(double chips) {
-        if (this.chips > 0) {
+        if (this.chips == 0) {
             this.chips = chips;
             price += this.chips;
         } else {
@@ -32,7 +32,7 @@ public class DeluxeHamburger extends Hamburger {
     }
 
     public void addDrinks(double drinks) {
-        if (this.drinks > 0) {
+        if (this.drinks == 0) {
             this.drinks = drinks;
             price += this.drinks;
         } else {
