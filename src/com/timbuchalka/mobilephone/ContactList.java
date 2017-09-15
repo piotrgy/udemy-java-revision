2a7731b2d1d8c5638fd3b96b52effacca5e6340a
piotrgy <piotr_gy@wp.pl>
2017-09-15 19:56:34 +0200

    Now list of contacts has getter and setter and it can add single contact an display them all.

---
+++ src/com/timbuchalka/mobilephone/ContactList.java
@@ -0,0 +1,43 @@
+package com.timbuchalka.mobilephone;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactList {
+    private List<Contact> contacts = new ArrayList<>();
+
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(List<Contact> contacts) {
+        this.contacts = contacts;
+    }
+
+    public void addContact(Contact contact) {
+        String contactName = contact.getName();
+        if (findContact(contactName) >=0) {
+            System.out.println("Contact: "+ contactName + "already exists, enter another name.");
+        }else {
+            contacts.add(contact);
+            System.out.println("Added contact: " + contactName);
+        }
+    }
+
+    public void displayContacts() {
+        int listSize = contacts.size();
+        System.out.println("You have " + listSize + " contacts in your address book.");
+        for (int i = 0; i < listSize; i++) {
+            System.out.println((i + 1) + ". " + contacts.get(i).getName() + ", number: " + contacts.get(i).getPhoneNumber());
+        }
+    }
+
+
+    private int findContact(String contactName) {
+        Contact searchedContact = new Contact();
+        searchedContact.setName(contactName);
+        return contacts.indexOf(searchedContact);
+    }
+
+
+}
