1ad33407c6021715d548d053a86a863a882f523f
piotrgy <piotr_gy@wp.pl>
2017-09-21 21:05:23 +0200

    Starting to build an app: displaying of menu, contacts and adding a contact is ready.

--- src/com/timbuchalka/mobilephone/MobileApp.java
+++ src/com/timbuchalka/mobilephone/MobileApp.java
@@ -1,9 +1,14 @@
 package com.timbuchalka.mobilephone;
 
 
+import java.util.Scanner;
+
 public class MobileApp {
+    private static Scanner scanner = new Scanner(System.in);
+    private static ContactList contactList = new ContactList();
+
     public static void main(String[] args) {
-        ContactList contactList = new ContactList();
+
         String[] names = {"Adam", "Ola", "Basia", "Janek"};
         String[] numbers = {"506233145", "505289145", "512233145", "533033945"};
 
@@ -25,6 +30,31 @@ public class MobileApp {
         contactList.removeContact("Janek");
         contactList.removeContact("Edek");
         contactList.displayContacts();*/
+
+        boolean quit = false;
+        int choice;
+
+        printInstructions();
+        while (!quit) {
+            System.out.println("Enter your choice: ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 0:
+                    printInstructions();
+                    break;
+                case 1:
+                    contactList.displayContacts();
+                    break;
+                case 2:
+                    addMobileContact();
+                    break;
+                case 6:
+                    quit = true;
+                    break;
+            }
+        }
     }
 
     public static void printInstructions() {
@@ -36,5 +66,15 @@ public class MobileApp {
         System.out.println("\t 4 - To remove a contact from the address book.");
         System.out.println("\t 5 - To search for a contact in the address book.");
         System.out.println("\t 6 - To quit the application.");
+    }
+
+    public static void addMobileContact() {
+        System.out.print("Please enter the contact's name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Please enter the contact's phone number: ");
+        String phoneNumber = scanner.nextLine();
+
+        contactList.addContact(new Contact(name, phoneNumber));
     }
 }
