e5cbf06cf1118e9a4fdd6daa40e0593b3b86bb6e
piotrgy <piotr_gy@wp.pl>
2018-01-22 21:21:23 +0100

    Constructor and getters for Album.

--- src/com/timbuchalka/musicplayer/Album.java
+++ src/com/timbuchalka/musicplayer/Album.java
@@ -5,4 +5,17 @@ import java.util.ArrayList;
 public class Album {
     private String title;
     private ArrayList<Song> songs;
+
+    public Album(String title) {
+        this.title = title;
+        this.songs = new ArrayList<>();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public ArrayList<Song> getSongs() {
+        return songs;
+    }
 }
