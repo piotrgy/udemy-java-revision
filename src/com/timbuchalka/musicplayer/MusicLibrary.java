8f7f30e49b2f0a71ecf88374be88d65f1d23f623
piotrgy <piotr_gy@wp.pl>
2018-01-22 21:22:38 +0100

    Initialising and getter for Musiclibrary.

--- src/com/timbuchalka/musicplayer/MusicLibrary.java
+++ src/com/timbuchalka/musicplayer/MusicLibrary.java
@@ -3,5 +3,9 @@ package com.timbuchalka.musicplayer;
 import java.util.ArrayList;
 
 public class MusicLibrary {
-    private ArrayList<Album> albums;
+    private ArrayList<Album> albums = new ArrayList<>();
+
+    public ArrayList<Album> getAlbums() {
+        return albums;
+    }
 }
