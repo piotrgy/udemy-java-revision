199b52764fe31b6a8c3c2039c93c3dbdf9026711
piotrgy <piotr_gy@wp.pl>
2018-01-26 21:26:07 +0100

    Building an album.

--- src/com/timbuchalka/musicplayer/Album.java
+++ src/com/timbuchalka/musicplayer/Album.java
@@ -6,7 +6,7 @@ public class Album {
     private String title;
     private ArrayList<Song> songs;
 
-    public Album(String title) {
+    private Album(String title) {
         this.title = title;
         this.songs = new ArrayList<>();
     }
@@ -17,5 +17,13 @@ public class Album {
 
     public ArrayList<Song> getSongs() {
         return songs;
+    }
+
+    public static Album buildAlbum (String title, String[] songsTitles, int[] sonngsDurations) {
+        Album album = new Album(title);
+        for (int i = 0; i < songsTitles.length; i++) {
+            album.songs.add(new Song(songsTitles[i], sonngsDurations[i]));
+        }
+        return album;
     }
 }
