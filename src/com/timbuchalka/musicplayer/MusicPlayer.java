8779fd26293cf884a642e16e3a7bb806679febbb
piotrgy <piotr_gy@wp.pl>
2018-01-30 20:30:42 +0100

    Method describing usual using of player (playing, skipping forward and backward, removing).

--- src/com/timbuchalka/musicplayer/MusicPlayer.java
+++ src/com/timbuchalka/musicplayer/MusicPlayer.java
@@ -1,6 +1,8 @@
 package com.timbuchalka.musicplayer;
 
 import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Scanner;
 
 //section 9, lecture 67 - LinkedList Challenge Part 1
 
@@ -34,10 +36,91 @@ public class MusicPlayer {
 
         musicPlayer.showMenu();
 
+
+    }
+
+    private void useThePlayer() {
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        boolean goingForward = true;
+        ListIterator<Song> listIterator = playlist.listIterator();
+
+        while (!quit) {
+            int action = scanner.nextInt();
+            scanner.nextLine();
+            switch (action) {
+                case 0:
+                    System.out.println("Turning off the player.");
+                    quit = true;
+                    break;
+
+                case 1:
+                    if (!goingForward) {
+                        if (listIterator.hasNext()) {
+                            listIterator.next();
+                        }
+                        goingForward = true;
+                    }
+                    if (listIterator.hasNext()) {
+                        System.out.println("Now playing " + listIterator.next());
+                    } else {
+                        System.out.println("Reached the end of the playlist");
+                        goingForward = false;
+                    }
+                    break;
+
+                case 2:
+                    if (goingForward) {
+                        if (listIterator.hasPrevious()) {
+                            listIterator.previous();
+                        }
+                        goingForward = false;
+                    }
+                    if (listIterator.hasPrevious()) {
+                        System.out.println("Now playing " + listIterator.previous());
+                    } else {
+                        System.out.println("We are at the start of the playlist");
+                        goingForward = true;
+                    }
+                    break;
+
+                case 3:
+                    if (goingForward) {
+                        if (listIterator.hasPrevious()) {
+                            System.out.println("Now playing " + listIterator.previous());
+                            listIterator.next();
+                        } else System.out.println("The track is no longer available.");
+                    } else {
+                        if (listIterator.hasNext()) {
+                            System.out.println("Now playing " + listIterator.next());
+                            listIterator.previous();
+                        } else System.out.println("The track is no longer available.");
+                    }
+                    break;
+
+                case 4:
+                    ListIterator<Song> displayListIterator = playlist.listIterator();
+                    int counter = 1;
+                    System.out.println("The songs on the playlist:");
+                    while (displayListIterator.hasNext()) {
+                        Song song = displayListIterator.next();
+                        System.out.println(" " + counter + ": " + song.getTitle() + " - " + song.getDuration() + " min");
+                        counter++;
+                    }
+
+                case 5:
+                    listIterator.remove();
+                    break;
+
+                case 6:
+                    showMenu();
+                    break;
+            }
+        }
     }
 
 
-    private void showMenu(){
+    private void showMenu() {
         System.out.println("Choose action:\npress ");
         System.out.println("0 - to quit\n" +
                 "1 - go to next song\n" +
