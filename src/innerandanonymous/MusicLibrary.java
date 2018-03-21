ede05fcc44de182ffab0b258895523c7bcd74c46
piotrgy <piotr_gy@wp.pl>
2018-03-21 20:39:57 +0100

    Copied all classes from the musicplayer package. In the Album class there is a inner class - SongList as a list of songs - instead of ArrayList.

---
+++ src/innerandanonymous/MusicLibrary.java
@@ -0,0 +1,11 @@
+package innerandanonymous;
+
+import java.util.ArrayList;
+
+public class MusicLibrary {
+    private ArrayList<Album> albums = new ArrayList<>();
+
+    public ArrayList<Album> getAlbums() {
+        return albums;
+    }
+}
