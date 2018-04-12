30cf40d8635918b3617b9b496dd700214cdaa8db
piotrgy <piotr_gy@wp.pl>
2018-04-12 20:45:17 +0200

    Concrete my own list class.

---
+++ src/com/timbuchalka/myownlist/NameListItem.java
@@ -0,0 +1,19 @@
+package com.timbuchalka.myownlist;
+
+public class NameListItem extends MyListItem {
+
+    public NameListItem(String name) {
+        super.setHeldValue(new String(name));
+    }
+
+    @Override
+    public int compareTo(MyListItem comparedItem) {
+        String heldValue = (String) this.getHeldValue();
+        return heldValue.compareToIgnoreCase((String) comparedItem.getHeldValue());
+    }
+
+    @Override
+    public boolean addInOrder(MyListItem newItem) {
+        return false;
+    }
+}
