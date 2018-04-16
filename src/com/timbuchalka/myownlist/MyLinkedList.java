d0c3fbbc45571f39573c003131595b61660b66a1
piotrgy <piotr_gy@wp.pl>
2018-04-16 20:51:05 +0200

    Information if the new item already exists.

--- src/com/timbuchalka/myownlist/MyLinkedList.java
+++ src/com/timbuchalka/myownlist/MyLinkedList.java
@@ -4,9 +4,13 @@ public class MyLinkedList {
     private MyListItem head;
 
     public boolean addInOrder(MyListItem newItem){
-        int comparing  = head.compareTo(newItem);
-        if (comparing == 0) return false;
+        int comparing = head.compareTo(newItem);
+        if (comparing == 0) {
+            System.out.println("The same item, as the new one already exists on the list.   ");
+            return false;
+        }
         else if(comparing > 0){
+            while (/*head.compareTo()*/){}
 
             return true;
         }
