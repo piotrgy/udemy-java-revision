874694bf343a86a0a4dcb7c639361c1ffc7f710c
piotrgy <piotr_gy@wp.pl>
2018-04-17 20:29:51 +0200

    If the current item doesn't exist, refer it to the new item.

--- src/com/timbuchalka/myownlist/MyLinkedList.java
+++ src/com/timbuchalka/myownlist/MyLinkedList.java
@@ -4,9 +4,14 @@ public class MyLinkedList {
     private MyListItem head;
 
     public boolean addInOrder(MyListItem newItem){
+        if(head == null) {
+            head = newItem;
+            System.out.println("This is first item added to the list.");
+            return true;
+        }
         int comparing = head.compareTo(newItem);
         if (comparing == 0) {
-            System.out.println("The same item, as the new one already exists on the list.   ");
+            System.out.println("The same item, as the new one already exists on the list.");
             return false;
         }
         else if(comparing > 0){
