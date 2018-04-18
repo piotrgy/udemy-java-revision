4502847cd58deedc06e6ea41d643a554cb8ff95e
piotrgy <piotr_gy@wp.pl>
2018-04-18 20:55:49 +0200

    If the new item is less than a current (first) item, add it before.

--- src/com/timbuchalka/myownlist/MyLinkedList.java
+++ src/com/timbuchalka/myownlist/MyLinkedList.java
@@ -3,23 +3,27 @@ package com.timbuchalka.myownlist;
 public class MyLinkedList {
     private MyListItem head;
 
-    public boolean addInOrder(MyListItem newItem){
-        if(head == null) {
+    public boolean addInOrder(MyListItem newItem) {
+        if (head == null) {
             head = newItem;
             System.out.println("This is first item added to the list.");
             return true;
         }
+
+        while (head.moveToPrevItem() != null) {
+            head = head.moveToPrevItem();
+        }
+
         int comparing = head.compareTo(newItem);
         if (comparing == 0) {
             System.out.println("The same item, as the new one already exists on the list.");
             return false;
         }
-        else if(comparing > 0){
-            while (/*head.compareTo()*/){}
-
+        else if (comparing > 0) {
+            head.moveToPrevItem().setNextItem(newItem);
+            newItem.setNextItem(head);
             return true;
-        }
-        else {
+        } else {
 
             return true;
         }
