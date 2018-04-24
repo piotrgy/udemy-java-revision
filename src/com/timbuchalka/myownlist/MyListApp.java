ff87475c2fe468b6d9d597fc9c9d99a8679d42d7
piotrgy <piotr_gy@wp.pl>
2018-04-24 14:00:12 +0200

    Testing of a list displaying and removing an item.

--- src/com/timbuchalka/myownlist/MyListApp.java
+++ src/com/timbuchalka/myownlist/MyListApp.java
@@ -4,13 +4,15 @@ public class MyListApp {
     public static void main(String[] args) {
         MyNameLinkedList nameLinkedList = new MyNameLinkedList();
 
-        String stringData = "Antek Janina Zenek Franek Andzia Justa";
+        String stringData = "Justa Zenek Antek Zenek Janina Franek Andzia";
         String[] data = stringData.split(" ");
 
         for (String s : data) {
             NameListItem nameListItem = new NameListItem(s);
             nameLinkedList.addInOrder(nameListItem);
         }
+        nameLinkedList.showAll();
+        nameLinkedList.remove();
         nameLinkedList.showAll();
 
     }
