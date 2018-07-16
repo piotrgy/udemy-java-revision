0fe171ecbca5aa1cd876877355eb22b27d178c41
piotrgy <piotr_gy@wp.pl>
2018-07-16 14:10:38 +0200

    Add info, printing of sorted teams.

--- src/com/timbuchalka/genericschallenge/League.java
+++ src/com/timbuchalka/genericschallenge/League.java
@@ -3,22 +3,30 @@ package com.timbuchalka.genericschallenge;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class League<T extends TeamSport>{
+public class League<T extends TeamSport> {
     private ArrayList<Team<T>> teams;
 
     public League() {
         teams = new ArrayList<>();
     }
 
-    boolean addTeam (Team<T> team){
-        if(teams.contains(team))
+    boolean addTeam(Team<T> team) {
+        if (teams.contains(team)) {
+            System.out.println("Team: " + team.getTeamName() + " already is in league.");
             return false;
+        }
         teams.add(team);
+        System.out.println("Team: " + team.getTeamName() + " added to league.");
         return true;
     }
 
-    public void printTeams(){
-        //Collections
+    public void printTeams() {
+        Collections.sort(teams);
+        int place = 1;
+        for (Team team: teams) {
+            System.out.println(place + ": " + team.getTeamName() + " - " + team.getRanking());
+            place++;
+        }
     }
 
 
