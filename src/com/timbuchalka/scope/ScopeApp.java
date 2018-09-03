115a531a28a974587915975f5b340f3ce0797d3f
piotrgy <piotr_gy@wp.pl>
2018-09-03 11:56:37 +0200

    Easiest solution without using scope.

--- src/com/timbuchalka/scope/ScopeApp.java
+++ src/com/timbuchalka/scope/ScopeApp.java
@@ -22,5 +22,25 @@ package com.timbuchalka.scope;
 // Change your program so that ALL variables (including the scanner
 // instance) are called x.
 
+import java.util.Scanner;
+
 public class ScopeApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a integer number: ");
+        int x = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("1 * " + x + " = " + 1 * x);
+        System.out.println("2 * " + x + " = " + 2 * x);
+        System.out.println("3 * " + x + " = " + 3 * x);
+        System.out.println("4 * " + x + " = " + 4 * x);
+        System.out.println("5 * " + x + " = " + 5 * x);
+        System.out.println("6 * " + x + " = " + 6 * x);
+        System.out.println("7 * " + x + " = " + 7 * x);
+        System.out.println("8 * " + x + " = " + 8 * x);
+        System.out.println("9 * " + x + " = " + 9 * x);
+        System.out.println("10 * " + x + " = " + 10 * x);
+        System.out.println("11 * " + x + " = " + 11 * x);
+        System.out.println("12 * " + x + " = " + 12 * x);
+    }
 }
